532585c0e6503dbbec6ce765a86dc546ee42099e
Sawako Sagliano <ssagli@uw.edu>
2019-06-14 16:51:26 -0700

    commiting project

---
+++ src/services/EventService.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL:         'http://localhost:3000',
+  withCredentials: false,
+  headers:         {
+    Accept:         'accplication/json',
+    'Content-Type': 'application/json'
+  }
+});
+
+export default {
+  getEvents() {
+    return apiClient.get('/events');
+  },
+
+  getEvent(id) {
+    return apiClient.get(`/events/${id}`);
+  }
+};
---
+++ src/views/EventList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>EventList</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+import EventService from '../services/EventService';
+import EventCard from '../components/EventCard.vue';
+  export default {
+    components: {
+      EventCard,
+    },
+    data() {
+      return {
+        events: []
+      }
+    },
+    created() {
+      EventService.getEvents()
+      .then(response => {
+        console.log(response.data);
+        this.events = response.data;
+      })
+      .catch(error => {
+        console.log('There was an error ', error)
+      })
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
